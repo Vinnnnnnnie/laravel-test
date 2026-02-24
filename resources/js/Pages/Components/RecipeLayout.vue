37811efbb9c20a9505ad0fef10a57fc6fd36c369
Vinnnnnnnie <vincentowens72@gmail.com>
2026-02-24 23:04:41 +0000

    extra success message

--- resources/js/Pages/Components/RecipeLayout.vue
+++ resources/js/Pages/Components/RecipeLayout.vue
@@ -41,16 +41,16 @@ const props = defineProps({
                     <Link class="btn flex flex-row gap-2" > Settings</Link>
                 </div>
             </div>
-            <div v-if="errors.length > 0" class="alert alert-danger">
-                <ul v-for='error in errors' class='px-4 py-2 bg-red-200 rounded-lg mb-2'>
-                    <li class='text-red-500'>{{ error }}</li>
-                </ul>
-            </div>
             <div class='grid grid-cols-12 divide-solid divide-x-1 divide-gray-500 justify-between gap-8 '>
                 <div class='col-start-2 col-span-10 flex-2 gap-4 bg-gray-200 dark:bg-gray-800 border-gray-500 p-4 border-x-1'>
                     <div v-if="errors"  class="alert alert-danger">
                         <ul v-for='error in errors' class='px-4 py-2 bg-red-200 rounded-lg mb-2'>
                             <li class='text-red-500'>{{ error }}</li>
+                        </ul>
+                    </div>
+                    <div v-if="success"  class="alert alert-danger">
+                        <ul v-for='message in success' class='px-4 py-2 bg-red-200 rounded-lg mb-2'>
+                            <li class='text-green-500'>{{ message }}</li>
                         </ul>
                     </div>
                     <slot />

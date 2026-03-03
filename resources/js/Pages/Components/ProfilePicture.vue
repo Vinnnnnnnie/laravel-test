d700dffe64150dfe76dd9d57a86f648694cf6061
Vinnnnnnnie <vincentowens72@gmail.com>
2026-03-03 22:13:16 +0000

    Forgot maxes on the profile picture on the top bar so it was taking up the entire page.

--- resources/js/Pages/Components/ProfilePicture.vue
+++ resources/js/Pages/Components/ProfilePicture.vue
@@ -11,7 +11,7 @@ const props = defineProps({
  });
 </script>
 <template>
-    <div :class="'dark:bg-gray-950 aspect-square overflow-hidden min-w-'+size+' min-h-'+size+' w-'+size+' h-'+size+' flex items-center justify-center bg-gray-50 align-items-center rounded-sm'">
+    <div :class="'dark:bg-gray-950 min-h-10 aspect-square overflow-hidden max-h-'+size+' max-w-'+size+' min-w-'+size+' min-h-'+size+' w-'+size+' h-'+size+' flex items-center justify-center bg-gray-50 align-items-center rounded-sm'">
         <img :src="route('image.users', image)" class='w-full h-full object-cover'>
     </div>
 </template>

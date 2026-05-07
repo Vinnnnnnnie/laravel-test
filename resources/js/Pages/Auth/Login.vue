70c8366ebc94cbdff46bda1e07ecdcb559f5993f
Vinnnnnnnie <vincentowens72@gmail.com>
2026-05-07 21:54:22 +0100

    Added guest login prompt on login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,22 +1,23 @@
 <script setup>
 import Layout from '../Components/Layout.vue'
-import { Form } from '@inertiajs/vue3'
+import { Form, Link } from '@inertiajs/vue3'
 </script>
 <template>
     <Layout>
         <div class='flex bg-gray-100 p-4 flex-col items-center dark:bg-gray-900 justify-self-center my-20'>
-        <h2>Login</h2>
-        <Form method="POST" action="/login" class='flex flex-col gap-4'>
-            <div class='flex flex-col'>
-                <label class='form-label' for="email">Email</label>
-                <input class='form-control' type="email" id="email" name="email" value='' required>
-            </div>
-            <div class='flex flex-col'>
-                <label for="password" class='form-label'>Password</label>
-                <input class='form-control' type="password" id="password" name="password" required>
-            </div>
-            <button type="submit" class='btn'>Login</button>
-        </Form>
-    </div>
+            <h2>Login</h2>
+            <Form method="POST" action="/login" class='flex flex-col gap-4'>
+                <div class='flex flex-col'>
+                    <label class='form-label' for="email">Email</label>
+                    <input class='form-control' type="email" id="email" name="email" value='' required>
+                </div>
+                <div class='flex flex-col'>
+                    <label for="password" class='form-label'>Password</label>
+                    <input class='form-control' type="password" id="password" name="password" required>
+                </div>
+                <button type="submit" class='btn'>Login</button>
+            </Form>
+            <Link class="rounded-2xl bg-blue-500 px-4 py-2 m-4 font-bold" :href="route('recipes.index')">Login as a Guest</Link>
+        </div>
     </Layout>
 </template>
